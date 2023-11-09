9fbae8fdb55adaa28b4a82efd9118555ecfa3ddd
Joon <bmp.tom@yahoo.com>
2023-11-09 13:23:51 +0900

    Update update_readme.py

--- .github/script/update_readme.py
+++ .github/script/update_readme.py
@@ -31,24 +31,36 @@ total_lines = sum(languages.values())
 
 # README.md 파일 생성 또는 업데이트
 with open('README.md', 'w', encoding='utf-8') as f:
+    # 포트폴리오 섹션 추가
     f.write("# 포트폴리오\n\n")
-    f.write("## 📚 기술 스택\n\n")
+    # 다국어 자기소개
+    f.write("## 🙋‍♂️ About Me\n\n")
+    f.write("안녕하세요, 저는 호준송입니다. 솔직한 마음을 표현하고, 다른 사람의 어려움을 이해하는 개발자입니다. 기술에 대한 깊은 이해와 함께 성장하고 싶은 동료들과 만나 성취를 즐기고 싶습니다.\n\n")
+    f.write("Hello, I am Hojoon Song, a developer who values honesty and understands the challenges others face. I look forward to growing alongside peers who share a deep understanding of technology and enjoying our achievements together.\n\n")
+    f.write("こんにちは、私はホジュンソンです。正直な心を表現し、他人の困難を理解する開発者です。技術に対する深い理解を共有し、一緒に成長して成果を楽しみたいと考えています。\n\n")
     
-    # 각 언어에 대한 badge와 함께 사용량 정보를 작성합니다.
+    # 기술 스택 섹션 추가
+    f.write("## 📚 Tech Stack\n\n")
+    # 언어별 뱃지 생성
     for language, lines in sorted(languages.items(), key=lambda x: x[1], reverse=True):
         percentage = (lines / total_lines) * 100
-        # URL 인코딩 적용
+        # URL 인코딩
         encoded_language = urllib.parse.quote(language)
         encoded_label = urllib.parse.quote(f"{language} - {lines} lines ({percentage:.2f}%)")
-        badge_url = f"https://img.shields.io/badge/{encoded_label}-informational?style=for-the-badge&logo={encoded_language.lower()}"
+        badge_url = f"https://img.shields.io/badge/{encoded_label}-informational?style=flat&logo={encoded_language.lower()}"
         f.write(f"![{language} Badge]({badge_url})\n")
-
-    # 표 형식으로 언어 사용량을 추가합니다.
-    f.write("\n\n## 언어 사용량\n\n")
-    f.write("| 언어 | 코드 라인 수 | 퍼센테이지 |\n")
-    f.write("| --- | ---: | ---: |\n")
-    for language, lines in sorted(languages.items(), key=lambda x: x[1], reverse=True):
-        percentage = (lines / total_lines) * 100
-        f.write(f"| {language} | {lines} | {percentage:.2f}% |\n")
-
-# 이 스크립트는 로컬에서 실행되며, 생성된 README.md 파일을 GitHub에 push해야 합니다.
+    
+    # GitHub 통계 섹션 추가
+    f.write("\n## 📈 GitHub Stats\n\n")
+    f.write("![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=hojoonSong&show_icons=true&layout=compact&theme=radical)\n\n")
+    f.write("![GitHub Stats](https://github-readme-stats.vercel.app/api?username=hojoonSong&show_icons=true&theme=radical)\n\n")
+    
+    # 블로그 및 소셜 미디어 링크 섹션 추가
+    f.write("## 📝 Blog\n\n")
+    f.write("[Visit my development blog](https://velog.io/@who_doctor)\n\n")
+    
+    f.write("## 🌐 Social\n\n")
+    # 여기에 소셜 미디어 링크 추가
+    # f.write("![Profile views](https://gpvc.arturio.dev/hojoonSong)\n")
+    
+    # 여기에 추가적인 소셜 뱃지를 추가할 수 있습니다.
